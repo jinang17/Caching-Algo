e85360406b4145f99e8cb73ef8e48c9d8f8f7475
Herculean17 <76151703+jinang17@users.noreply.github.com>
2022-12-05 11:12:04 +0530

    LRU cache implemented

---
+++ LRU.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class LRUCache
+{
+public:
+    class node
+    {
+    public:
+        int key;
+        int val;
+        node *prev;
+        node *next;
+        node(int key, int val)
+        {
+            this->key = key;
+            this->val = val;
+        }
+    };
+    node *head = new node(-1, -1);
+    node *tail = new node(-1, -1);
+    int capacity;
+    unordered_map<int, node *> map;
+    LRUCache(int capacity)
+    {
+        this->capacity = capacity;
+        head->next = tail;
+        tail->prev = head;
+    }
+    void deleteNode(node *curNode)
+    {
+        node *prevNode = curNode->prev;
+        node *nextNode = curNode->next;
+        prevNode->next = nextNode;
+        nextNode->prev = prevNode;
+    }
+    void addNode(node *curNode)
+    {
+        curNode->next = head->next;
+        curNode->prev = head;
+        curNode->next->prev = curNode;
+        head->next = curNode;
+    }
+    int get(int key)
+    {
+        if (map.find(key) == map.end())
+            return -1;
+        node *curNode = map[key];
+        int result = curNode->val;
+        map.erase(key);
+        deleteNode(curNode);
+        addNode(curNode);
+        map[key] = head->next;
+        return result;
+    }
+
+    void put(int key, int value)
+    {
+        if (map.find(key) != map.end())
+        {
+            node *curNode = map[key];
+            map.erase(key);
+            deleteNode(curNode);
+        }
+        if (map.size() == capacity)
+        {
+            map.erase(tail->prev->key);
+            deleteNode(tail->prev);
+        }
+        addNode(new node(key, value));
+        map[key] = head->next;
+    }
+};
+
+int main()
+{
+    LRUCache c(2);
+    c.put(1, 1);
+    c.put(2, 2);
+    cout << c.get(1) << endl;
+    c.put(3, 3);
+    cout << c.get(2) << endl;
+    c.put(4, 4);
+    cout << c.get(1) << endl;
+    cout << c.get(3) << endl;
+}
+
+
+
